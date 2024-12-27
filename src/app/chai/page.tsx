b1ede5dfb9c68219440e74470687606d85bcd14f
Kabeer Chaturvedi <kbrchaturvedi@gmail.com>
2024-12-27 16:38:07 +0530

    Installed next and shadcn/ui

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,7 @@
+const Chaipage = () => {
+  return <div className="h-full flex justify-center items-center flex-col">Chaipage
+  <button className="px-6 py-2 bg-blue-500 rounded my-3 hover:bg-blue-700">Test Button</button>
+  </div>;
+};
+
+export default Chaipage;
